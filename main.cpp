e08c4c06c9b666a2bde2971c69e7727355ef4d33
Albin Fyhr <albinfyhr@gmail.com>
2023-03-29 00:20:30 +0200

    Update main.cpp with swap buffers

--- main.cpp
+++ main.cpp
@@ -10,7 +10,14 @@ int main()
     while (!glfwWindowShouldClose(renderer.window))
     {
         renderer.Render();
+ 
+        // Swap the front and back buffers
+        glfwSwapBuffers(renderer.window);
+
+        // Poll for and process events
+        glfwPollEvents();
     }
 
+    glfwTerminate();
     return 0;
 }
