f4f5d4fbca73929a9a3939bda62c391de388020d
Albin Fyhr <albinfyhr@gmail.com>
2023-03-28 14:36:22 +0200

    Added shaders, GLFW and GLEW

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <debug/test.h>
+#include <glew/include/GL/glew.h>
+#include <glfw/include/GLFW/glfw3.h>
 
 using namespace std;
 
@@ -14,6 +16,9 @@ int main()
         cout << word << " ";
     }
     cout << endl;
+
+    GLuint vao = 2;
+    cout << vao << endl;
 
     cout << fname(1, 2, 3) << endl;
     int x;
