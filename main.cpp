2cc3c35c601124d47f44bf90c83ca17ae11b388a
Albin Fyhr <albinfyhr@gmail.com>
2023-03-28 15:44:11 +0200

    Fixed glew (?)

--- main.cpp
+++ main.cpp
@@ -27,6 +27,31 @@ int main()
 
     // Initialize GLEW
     GLenum glewInitResult = glewInit();
+    if (glewInitResult != GLEW_OK)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Set the OpenGL viewport
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear the screen
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Clean up
+    glfwTerminate();
 
     return 0;
 }
