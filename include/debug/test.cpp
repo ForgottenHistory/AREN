93b5fe54d60c79b1fb7e3e54ce981cbab3089a11
Albin Fyhr <albinfyhr@gmail.com>
2023-03-28 11:34:17 +0200

    Added test cpp/h

---
+++ include/debug/test.cpp
@@ -0,0 +1,5 @@
+#include <test.h>
+
+int fname( int x, int y, int z ) {
+    return x + y + z;
+}
---
+++ include/debug/test.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef TEST_H
+#define TEST_H
+
+// Declare the square function
+int fname( int x, int y, int z );
+
+#endif
+
