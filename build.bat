2c2e033da373b2e7e7a27b13652287e8c2d29e76
Albin Fyhr <albinfyhr@gmail.com>
2023-03-28 14:09:29 +0200

    updated tasks and build to work
    finally

--- build.bat
+++ build.bat
@@ -1,25 +1,16 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set INPUT_FILE=main.cpp
-set INCLUDE_FOLDER="./include"
-set OUTPUT_FILE=main.exe
+set INCLUDE_FOLDER=%1
+set OUTPUT_FILE=%2
 
 set "CPPFiles="
-set "headerIncludes="
 
-for /r %INCLUDE_FOLDER% %%f in (*.h;*.hpp) do (
-    set "headerIncludes=!headerIncludes! -I"%%f""
-)
-
-for /r %INCLUDE_FOLDER% %%f in (*.cpp) do (
+for /r %%f in (*.cpp) do (
     set "CPPFiles=!CPPFiles! "%%f""
 )
-
-echo Header Includes:
-echo %headerIncludes%
 
 echo List of CPP files:
 echo %CPPFiles%
 
-g++ %INPUT_FILE% %CPPFiles% %headerIncludes% -o %OUTPUT_FILE% -lstdc++ -I%INCLUDE_FOLDER%
+g++ %CPPFiles% -o %OUTPUT_FILE% -lstdc++ -I"%INCLUDE_FOLDER%"
