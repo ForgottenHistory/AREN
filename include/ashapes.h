3ded6428bdded2bd430ecaba8bb27f04b248dd88
Albin Fyhr <albinfyhr@gmail.com>
2023-03-29 17:27:22 +0200

    Create ashapes.h

---
+++ include/ashapes.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+
+class APrimitiveShape {
+public:
+    // Virtual destructor to ensure proper cleanup of derived classes.
+    virtual ~APrimitiveShape() {}
+
+    // Initialize and set up shape data for rendering.
+    virtual void Setup() = 0;
+
+    // Render the shape.
+    virtual void Render() = 0;
+
+protected:
+    // Function to generate and bind VAO, VBO, and EBO.
+    void setupBuffers();
+
+    // Generate vertex data for the shape.
+    virtual std::vector<GLfloat> generateVertexData() const = 0;
+
+    // Generate index data for the shape.
+    virtual std::vector<GLuint> generateIndexData() const = 0;
+
+    // Vertex Array Object (VAO) for the shape.
+    GLuint VAO;
+
+    // Vertex Buffer Object (VBO) for the shape.
+    GLuint VBO;
+
+    // Element Buffer Object (EBO) for the shape.
+    GLuint EBO;
+};
