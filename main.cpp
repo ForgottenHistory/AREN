efc7b19d4a88844f187f4805c86caab04463da4a
Albin Fyhr <albinfyhr@gmail.com>
2023-03-29 06:35:23 +0200

    Update main.cpp with Timer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <render/arenderer.h>
+#include <time.h>
+#include <cmath>
 
 using namespace std;
 
@@ -7,10 +9,21 @@ int main()
     // Create a renderer
     ARenderer renderer;
 
+    float printTime = 1.0f;
     while (!glfwWindowShouldClose(renderer.window))
     {
+        Time::UpdateTime();
+
         renderer.Render();
- 
+
+        float elapsedTime = Time::elapsedTime;
+
+        if (elapsedTime >= printTime)
+        {
+            Time::PrintTime();
+            printTime = elapsedTime + 1.0f;
+        }
+
         // Swap the front and back buffers
         glfwSwapBuffers(renderer.window);
 
