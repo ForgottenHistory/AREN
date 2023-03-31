410db7e900d0e943825ea752ec5f47486ccd7a0a
Albin Fyhr <albinfyhr@gmail.com>
2023-03-31 19:15:10 +0200

    Update aobject.cpp

--- include/aobject.cpp
+++ include/aobject.cpp
@@ -31,9 +31,7 @@ ACube::ACube()
     AddComponent(meshComponent);
 
     // Initialize the transform component
-    transform = new ATransform(  glm::vec3 (0.0f, 0.0f, -1.0f),
-                                 glm::vec3 (0.0f, 0.0f, 0.0f),
-                                 glm::vec3 (1.0f, 1.0f, 1.0f) );
+    transform = new ATransform();
     AddComponent(transform);
 }
 
