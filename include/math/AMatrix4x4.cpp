524c675fd00d5b8e716873335dccc221355c3110
Albin Fyhr <albinfyhr@gmail.com>
2023-04-17 15:04:44 +0200

    Created Matrix4x4 class

---
+++ include/math/AMatrix4x4.cpp
@@ -0,0 +1,57 @@
+#include "AMatrix4x4.h"
+
+AMatrix4x4::AMatrix4x4()
+{
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            m[i][j] = 0.0f;
+        }
+    }
+}
+
+AMatrix4x4 AMatrix4x4::Identity()
+{
+    AMatrix4x4 identity;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        identity.m[i][i] = 1.0f;
+    }
+
+    return identity;
+}
+
+AMatrix4x4 AMatrix4x4::operator*(const AMatrix4x4& other) const
+{
+    AMatrix4x4 result;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            for (int k = 0; k < 4; ++k)
+            {
+                result.m[i][j] += m[i][k] * other.m[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+AMatrix4x4& AMatrix4x4::operator*=(const AMatrix4x4& other)
+{
+    AMatrix4x4 result = *this * other;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            m[i][j] = result.m[i][j];
+        }
+    }
+
+    return *this;
+}
---
+++ include/math/AMatrix4x4.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class AMatrix4x4
+{
+public:
+    float m[4][4];
+
+    AMatrix4x4();
+
+    // Identity matrix
+    static AMatrix4x4 Identity();
+
+    // Matrix multiplication
+    AMatrix4x4 operator*(const AMatrix4x4& other) const;
+
+    // Compound assignment operator for matrix multiplication
+    AMatrix4x4& operator*=(const AMatrix4x4& other);
+};
