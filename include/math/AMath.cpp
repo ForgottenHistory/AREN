62aa90c3e70fc14e62d90959d3eede78d2abd5d4
Albin Fyhr <albinfyhr@gmail.com>
2023-04-17 15:11:30 +0200

    Created AMath functions

---
+++ include/math/AMath.cpp
@@ -0,0 +1,31 @@
+#include "AMath.h"
+
+float ASin(float x)
+{
+    return std::sin(x);
+}
+
+float ACos(float x)
+{
+    return std::cos(x);
+}
+
+float ATan(float x)
+{
+    return std::atan(x);
+}
+
+float ATan2(float y, float x)
+{
+    return std::atan2(y, x);
+}
+
+float ASqrt(float x)
+{
+    return std::sqrt(x);
+}
+
+float APow(float x, float y)
+{
+    return std::pow(x, y);
+}
---
+++ include/math/AMath.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <cmath>
+
+// Trigonometric functions
+float ASin(float x);
+float ACos(float x);
+float ATan(float x);
+float ATan2(float y, float x);
+
+// Other utility functions
+float ASqrt(float x);
+float APow(float x, float y);
