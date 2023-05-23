789c4785dcf49c6f9bf7f890150634dbc609a589
Albin Fyhr <albinfyhr@gmail.com>
2023-05-23 20:25:38 +0200

    More Matrix constructors

--- include/math/AMatrix4x4.h
+++ include/math/AMatrix4x4.h
@@ -3,12 +3,20 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+class AVec3;
 class AMatrix4x4
 {
 public:
     float m[4][4];
 
     AMatrix4x4();
+    AMatrix4x4(float value);
+    AMatrix4x4(float& m00, float& m01, float& m02, float& m03,
+               float& m10, float& m11, float& m12, float& m13,
+               float& m20, float& m21, float& m22, float& m23,
+               float& m30, float& m31, float& m32, float& m33);
+    AMatrix4x4(const AVec3& translation, const AVec3& rotation, const AVec3& scale);
+    AMatrix4x4(const glm::mat4& glmMatrix);
 
     // Identity matrix
     static AMatrix4x4 Identity();
